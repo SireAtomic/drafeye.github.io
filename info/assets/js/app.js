d5ae54edda82ca80bca35b2d1a5228576a75bdab
Drafi <68693724+drafeye@users.noreply.github.com>
2021-02-13 10:17:43 +0000

    Delete info directory

--- info/assets/js/app.js
+++
@@ -1,6 +0,0 @@
-import * as particlesJS from 'particles.js';
-import { myFirstParticle } from './particles.ts'
-
-particlesJS.load('particles-js', myFirstParticle, function() {
-  console.log('callback - particles.js config loaded');
-});
